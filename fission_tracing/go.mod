a1995b6f582f8886fbcf453de9f887b774dc2db5
DayDreumer <dongyinGuan@outlook.com>
2023-03-30 21:07:54 +0800

    add random generate function in id_generator.

---
+++ fission_tracing/go.mod
@@ -0,0 +1,5 @@
+module github.com/DayDreumer/FissionRelatedCode/trace
+
+go 1.15
+
+require github.com/DayDreumer/FissionRelatedCode v0.0.0-20230330063848-3256d66b3c5c // indirect
--- fission_tracing/trace/id_generator.go
+++ fission_tracing/trace/id_generator.go
@@ -3,6 +3,8 @@ package trace
 import (
 	"math/rand"
 	"sync"
+
+	"github.com/DayDreumer/FissionRelatedCode/fission_tracing/trace"
 )
 
 type randomGenerator struct {
@@ -10,7 +12,18 @@ type randomGenerator struct {
 	ran rand.Rand
 }
 
-func (rg *randomGenerator) generateTraceID() ([]byte, error) {
-	// var traceID TraceID
+func (rg *randomGenerator) generateTraceID() trace.TraceID {
+	traceID := trace.TraceID{}
+	_, _ = rg.ran.Read(traceID[:])
+	return traceID
+}
 
+func (rg *randomGenerator) generateSpanID() trace.SpanID {
+	spanID := trace.SpanID{}
+	_, _ = rg.ran.Read(spanID[:])
+	return spanID
+}
+
+func (rg *randomGenerator) generateID() (trace.TraceID, trace.SpanID) {
+	return rg.generateTraceID(), rg.generateSpanID()
 }
