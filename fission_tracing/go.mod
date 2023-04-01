d040a99e2ebdf907b0092c63e43e8759d689a836
DayDreumer <dongyinGuan@outlook.com>
2023-04-01 15:42:13 +0800

    add util package to generate random id and convert id format.

--- fission_tracing/go.mod
+++ fission_tracing/go.mod
@@ -2,4 +2,4 @@ module github.com/DayDreumer/FissionRelatedCode/trace
 
 go 1.15
 
-require github.com/DayDreumer/FissionRelatedCode v0.0.0-20230330063848-3256d66b3c5c // indirect
+require github.com/DayDreumer/FissionRelatedCode v0.0.0-20230330063848-3256d66b3c5c
---
+++ fission_tracing/trace/util.go
@@ -0,0 +1,71 @@
+package trace
+
+import (
+	crand "crypto/rand"
+	"encoding/binary"
+	"encoding/hex"
+	"encoding/json"
+	"math/rand"
+	"sync"
+
+	"github.com/DayDreumer/FissionRelatedCode/fission_tracing/trace"
+)
+
+func TraceidToString(traceID trace.TraceID) string {
+	recv := make([]byte, 16)
+	recv = traceID[:]
+	return hex.EncodeToString(recv)
+}
+
+func SpanidToString(spanID trace.SpanID) string {
+	recv := make([]byte, 8)
+	recv = spanID[:]
+	return hex.EncodeToString(recv)
+}
+
+type RandomGenerator struct {
+	mu  sync.Mutex
+	ran *rand.Rand
+}
+
+func NewRandomGenerator() RandomGenerator {
+	rg := RandomGenerator{}
+	var rgSeed int64
+	_ = binary.Read(crand.Reader, binary.LittleEndian, &rgSeed)
+	rg.ran = rand.New(rand.NewSource(rgSeed))
+	return rg
+}
+
+func (rg *RandomGenerator) generateTraceID() trace.TraceID {
+	traceID := trace.TraceID{}
+	_, _ = rg.ran.Read(traceID[:])
+	return traceID
+}
+
+func (rg *RandomGenerator) generateSpanID() trace.SpanID {
+	spanID := trace.SpanID{}
+	_, _ = rg.ran.Read(spanID[:])
+	return spanID
+}
+
+func (rg *RandomGenerator) generateID() (trace.TraceID, trace.SpanID) {
+	return rg.generateTraceID(), rg.generateSpanID()
+}
+
+type Coder struct {
+	Type string
+	// Object json.RawMessage
+	ParentSpanID SpanID
+}
+
+func (c *Coder) encode() string {
+	res, err := json.Marshal(c)
+	if err != nil {
+		return "fail"
+	}
+	return string(res)
+}
+
+func (c *Coder) decode(val string) {
+	json.Unmarshal([]byte(val), c)
+}
