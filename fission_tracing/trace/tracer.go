24b60c316f2d664659e6c5c324189b38c8216f12
DayDreumer <dongyinGuan@outlook.com>
2023-04-13 21:15:04 +0800

    first version of Tracer and add Tracer's functions.

--- fission_tracing/trace/tracer.go
+++ fission_tracing/trace/tracer.go
@@ -1,15 +1,71 @@
 package trace
 
-import "context"
+import (
+	"context"
+	"time"
 
-type tracer struct {
-	name string
+	"fission.tracing/tag"
+)
+
+type Tracer struct {
+	Name string
 }
 
-func (tr *tracer) Start(name string, ctx context.Context) (context.Context, string) {
+// start a new span
+func (tr *Tracer) Start(name string, ctx context.Context) (context.Context, CommonSpan) {
 	if ctx == nil {
 		ctx = context.Background()
 	}
-	tr.name = name
+
+	// if lastSpan exists, current span should be its new child.
+	if lastSpan := GetLastSpanFromContext(ctx); lastSpan.Operatorname != "none" {
+		lastSpan.childSpanCount++
+		ctx = InheritParentContext(ctx, lastSpan)
+	}
+
+	newSpan := tr.getNewSpan(ctx, name)
+	return InheritParentContext(ctx, newSpan), newSpan
+}
+
+func (tr *Tracer) getNewSpan(ctx context.Context, name string) CommonSpan {
+	//	get last SpanContext to inherit its traceID
+	var parentSC SpanContext = GetLastSpanContextFromContext(ctx)
+	var tid TraceID
+	var sid SpanID
+	ig := NewRandomGenerator()
+	if parentSC.TraceID().Exist() {
+		tid = parentSC.TraceID()
+		sid = ig.generateSpanID()
+	} else {
+		tid, sid = ig.generateID()
+	}
+
+	// construct new SpanContextInfo
+	sci := SpanContextInfo{
+		TraceID:        tid,
+		SpanID:         sid,
+		RemotelyCalled: parentSC.remotelyCalled,
+	}
+	// making sure whether parent's SpanID Exists
+	if parentSC.parentSpanID.Exist() {
+		sci.ParentSpanID = parentSC.parentSpanID
+	}
+	sc := sci.ConvertToSpanContext()
+	newSpan := CommonSpan{
+		Operatorname:      name,
+		startTime:         time.Now(),
+		traceTag:          tag.NewTagDict(),
+		spanContext:       sc,
+		parentSpanContext: parentSC,
+		childSpanCount:    0,
+	}
+	return newSpan
+}
+
+func (tr *Tracer) ExtractSpanContext() {
+
+}
+
+func (tr *Tracer) InjectSpanContext() {
 
 }
