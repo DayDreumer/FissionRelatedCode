20815625e87ac121377fd50c42ba62011428f910
DayDreumer <dongyinGuan@outlook.com>
2023-05-01 21:41:23 +0800

    fix bug.

--- fission_tracing/trace/porpagate.go
+++ fission_tracing/trace/porpagate.go
@@ -2,14 +2,6 @@ package trace
 
 type UniResponse struct {
 	TraceInfo string
-	status    int
-	value     int
-}
-
-func (ur UniResponse) GetStatus() int {
-	return ur.status
-}
-
-func (ur UniResponse) GetValue() int {
-	return ur.value
+	Status    int
+	Value     int
 }
